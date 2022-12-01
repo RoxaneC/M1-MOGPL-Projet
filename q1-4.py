cfcc7b30ce52a13e6645e2a025dd24f82b380d68
RoxaneC <61699049+RoxaneC@users.noreply.github.com>
2022-12-02 00:04:33 +0100

    Update q1-4.py

--- q1-4.py
+++ q1-4.py
@@ -9,74 +9,69 @@ Created on Thu Dec  1 16:16:41 2022
 
 from gurobipy import *
 
+
 nbcont=5
 nbvar=11
 
-# intervalles de nos variables
+# Intervalles de nos variables
 lignes = range(nbcont)
-colonnes_bik = [1,2,4,5]
+colonnes = range(nbvar)
+
+# Explicitation des colonnes représentants les variables rk, bik et xi
 colonnes_rk = [0,3]
+colonnes_bik = [1,2,4,5]
 colonnes_x = [6,7,8,9,10]
-colonnes = [0,1,2,3,4,5,6,7,8,9,10]
 
 # Matrice des contraintes
-
 a = [[1, -1,  0,  0,  0,  0,  -5,  -6,  -4,  -8,  -1],
      [1,  0, -1,  0,  0,  0,  -3,  -8,  -6,  -2,  -5],
      [0,  0,  0,  1, -1,  0,  -5,  -6,  -4,  -8,  -1],
      [0,  0,  0,  1,  0, -1,  -3,  -8,  -6,  -2,  -5],
      [0,  0,  0,  0,  0,  0,   1,   1,   1,   1,   1]]
 
-
 # Second membre
-
 b = [0,0,0,0, 3]
 
-# Coefficients de la fonction objectif
-
+# Coefficients de la fonction objective
 c = [1,-1,-1,2,-1,-1,0,0,0,0,0]
 
+# Création de modèle
 m = Model("mogplex")     
         
-# declaration variables de decision
-
+# Déclaration variables de décision
 x = []
 for i in colonnes:
+     # les rk sont réels
     if i in colonnes_rk:
         x.append(m.addVar(vtype=GRB.CONTINUOUS, lb=-GRB.INFINITY, name="r%d" % (i+1)))
+    
+    # les bik sont supérieurs ou égaux à 0
     if i in colonnes_bik:
         x.append(m.addVar(vtype=GRB.CONTINUOUS, lb=0, name="b%d" % (i+1)))
+          
+    # les xi sont binaires (1 ou 0)
     if i in colonnes_x:
         x.append(m.addVar(vtype=GRB.BINARY, name="x%d" % (i+1)))
-        
 
-#for i1 in colonnes_bik:
-#    x[i1] = m.addVar(vtype=GRB.CONTINUOUS, lb=0, name="x%d" % (i1+1))
-#for i2 in colonnes_rk:
-#    x[i2] = m.addVar(vtype=GRB.CONTINUOUS, lb=-GRB.INFINITY, name="x%d" % (i2+1))
-
-
-# maj du modele pour integrer les nouvelles variables
+     
+# maj du modèle pour integrer les nouvelles variables
 m.update()
 obj = LinExpr();
 obj = 0
 for j in colonnes:
     obj += c[j] * x[j]
         
-# definition de l'objectif
-
+# definition de l'objectif (maximisation de la fonction objectif)
 m.setObjective(obj,GRB.MAXIMIZE)
 
 # Definition des contraintes
-
 for i in lignes:
     m.addConstr(quicksum(a[i][j]*x[j] for j in colonnes) <= b[i], "Contrainte%d" % i)
     
 # Resolution
-
 m.optimize()
 
-
+# Affichage des résultats
 print("")                
 print('Solution optimale:')
 for j in colonnes:
